57db6911f96ad167b7c9c10a9e745256db8c68a3
sanqian <2531607953@qq.com>
2023-05-14 14:34:42 +0200

    hasaccess

--- ACS.py
+++ ACS.py
@@ -1,3 +1,4 @@
+import json
 import mysql.connector
 #def main():
 try:
@@ -19,11 +20,19 @@ if(acscursor.rowcount<4): #The database should contains 4 tables
     print("The database is broken, please recreate the database with the ACS.sql file!")
     exit()
 
-acscursor.execute("CALL addReader((SELECT JSON_ARRAY(1,2,3,4)))")
-acsDB.commit()
-acscursor.execute('SELECT * FROM `Readers`')
-result = acscursor.fetchall()
-for x in result:
-    print(x)
-
+# acscursor.execute("CALL addRole('Skolledning')")
+# acscursor.execute("CALL addRole('Teacher')")
+# acscursor.execute("CALL addRole('Student')")
+# acsDB.commit()
+# acscursor.execute('SELECT * FROM `RoleGroups`')
+# result = acscursor.fetchall()
+# for x in result:
+#     print(x)
+# acscursor.execute("CALL addUser('Christian Nordahl',JSON_ARRAY(1,2))")
+# acscursor.execute("CALL addUser('Anders Carlsson',JSON_ARRAY(2))")
+# acscursor.execute("CALL addUser('Xiao Zhu',JSON_ARRAY(3))")
+# acsDB.commit()
+acscursor.execute('SELECT userAssignedGroups(1)')
+result = acscursor.fetchone()
+print(tuple(json.loads(result[0])))
 #main()
