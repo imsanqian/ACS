6e4eeeb0022fec53d716343a0c3485952e056d56
sanqian <2531607953@qq.com>
2023-05-15 14:16:37 +0200

    Menu

--- ACS.py
+++ ACS.py
@@ -1,24 +1,30 @@
 import json
 import mysql.connector
-#def main():
-try:
-    acsDB = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root",
-    )
-    print("Connected to the database.")
-except:
-    raise Exception("Failed to connect the database")
-    exit()
-acscursor = acsDB.cursor()
-acscursor.execute('CREATE DATABASE IF NOT EXISTS acs')
-acscursor.execute('USE acs')
-acscursor.execute('SHOW TABLES')
-result = acscursor.fetchall()
-if(acscursor.rowcount<4): #The database should contains 4 tables
-    print("The database is broken, please recreate the database with the ACS.sql file!")
-    exit()
+
+C_WHITE = "\033[37m"
+C_PURPLE = "\033[95m"
+C_RED = "\033[91m"
+
+def connectDB():
+    try:
+        global acsDB 
+        acsDB= mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root",
+        )
+        print("Connected to the database.")
+    except:
+        raise Exception("Failed to connect the database")
+    global acscursor
+    acscursor = acsDB.cursor()
+    acscursor.execute('CREATE DATABASE IF NOT EXISTS acs')
+    acscursor.execute('USE acs')
+    acscursor.execute('SHOW TABLES')
+    result = acscursor.fetchall()
+    if(acscursor.rowcount<4): #The database should contains 4 tables
+        print("The database is broken, please recreate the database with the ACS.sql file!")
+        exit()
 
 # acscursor.execute("CALL addRole('Skolledning')")
 # acscursor.execute("CALL addRole('Teacher')")
@@ -32,7 +38,120 @@ if(acscursor.rowcount<4): #The database should contains 4 tables
 # acscursor.execute("CALL addUser('Anders Carlsson',JSON_ARRAY(2))")
 # acscursor.execute("CALL addUser('Xiao Zhu',JSON_ARRAY(3))")
 # acsDB.commit()
-acscursor.execute('SELECT userAssignedGroups(1)')
-result = acscursor.fetchone()
-print(tuple(json.loads(result[0])))
+# acscursor.execute('SELECT userAssignedGroups(1)')
+# result = acscursor.fetchone()
+# print(tuple(json.loads(result[0])))
 #main()
+
+def main():
+    connectDB()
+    print("*This project is in purpose of application of Mysql")
+    print("----Welcome to the Access Control System Demo----")
+    selectInMain()
+
+def selectInMain():
+    printMainMenu()
+    selected = input("> ") 
+    selected = int(selected) if selected.isdigit() else -1
+    match selected:
+        case 1:
+            selectInAdmin()
+        case 9:
+            print("Good Bye!")
+            exit()
+        case _:
+            print("Unkown choise, select again")
+            selectInMain()
+
+def selectInAdmin():
+    printAdminMenu()
+    selected = input("> ")
+    selected = int(selected) if selected.isdigit() else -1
+    selected = int(selected)
+    match selected:
+        case 0:
+            selectInMain()
+        case 1:
+            result = selectAllFromDB("Readers")
+            print("-----Card Readers------")
+            print("ID\tAccess Groups\tAccess Users")
+            for x in result:
+                print(str(x[0])+"\t"+str(x[1])+"\t"+str(x[2]))
+        case 2:
+            result = selectAllFromDB("Users")
+            print("-----Users------")
+            print("ID\tName\t\t\t\tRegistered Date\t\tAssigned Groups")
+            for x in result:
+                print(str(x[0])+"\t"+str(x[1])+"\t\t\t"+str(x[2])+"\t"+str(x[3]).expandtabs(16))
+        case 3:
+            result = selectAllFromDB("RoleGroups")
+            print("-----Role Groups------")
+            print("ID\tRole Name")
+            for x in result:
+                print(str(x[0])+"\t"+str(x[1]))
+        case 5:
+            print("---Create a Card Reader---")
+            print("You have to enter the group ids which have access to pass this door\n\
+Leave it blank if you want to cancel")
+            groups = input("Groups that has access: ")
+            if len(groups) != 0:
+                acscursor.execute(f"CALL addReader((SELECT JSON_ARRAY({groups})))")
+                acsDB.commit()
+                acscursor.execute("SELECT * FROM readers ORDER BY `readerID` DESC LIMIT 1;")
+                result = acscursor.fetchone()
+                print("A new reader created: "+str(result))
+        case 6:
+            print("---Add a User---")
+            print("You have to enter user name and the group ids which this user is assigned to\n\
+Leave it blank if you want to cancel")
+            uname = input("User name: ")
+            if len(uname) != 0:
+                groups = input("Groups this user belongs: ")
+                if len(groups) != 0:
+                    acscursor.execute(f"CALL addUser('{uname}',(SELECT JSON_ARRAY({groups})))")
+                    acsDB.commit()
+                    acscursor.execute("SELECT * FROM users ORDER BY `userID` DESC LIMIT 1;")
+                    result = acscursor.fetchone()
+                    print("A new user added: "+str(result))
+        case 7:
+            print("---Add a Role---")
+            print("You have to enter group name\n\
+Leave it blank if you want to cancel")
+            gname = input("Group name: ")
+            if len(gname) != 0:
+                acscursor.execute(f"CALL addRole('{gname}')")
+                acsDB.commit()
+                acscursor.execute("SELECT * FROM rolegroups ORDER BY `groupID` DESC LIMIT 1;")
+                result = acscursor.fetchone()
+                print("A new role added: "+str(result))
+        case _:
+            print("Unkown choise, select again")
+    selectInAdmin()
+
+def printMainMenu():
+    print("-----------Menu------------")
+    print("1. Administrator Panel")
+    print("2. Blip my card")
+    print(C_PURPLE+"9. Quit"+C_WHITE)
+    print("---------------------------")
+
+def printAdminMenu():
+    print("-----------Admin Menu------------")
+    print(C_PURPLE+"0. Previous Menu"+C_WHITE)
+    print("1. Show all Card Readers")
+    print("2. Show all Users")
+    print("3. Show all Roles")
+    print("4. Search Log")
+    print("5. Create Card Reader")
+    print("6. Create User")
+    print("7. Create Role")
+    print(C_RED+"8. Delete Card Reader")
+    print("9. Delete User")
+    print("10. Delete Role"+C_WHITE)
+    print("-------------------------------")
+
+def selectAllFromDB(tbName:str):
+    acscursor.execute("SELECT * FROM "+tbName)
+    return acscursor.fetchall()
+main()
+
